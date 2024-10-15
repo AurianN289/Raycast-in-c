7befb19bd69e519ac201e6c1f3eacb5c55c55820
Aurian Bastos <aurianacimento0@gmail.com>
2024-10-15 10:10:45 -0300

    relação mapa e buffer

--- top-down/top_down.c
+++ top-down/top_down.c
@@ -10,7 +10,7 @@ char void_char = '.';
 char wall_char = '%';
 
 int block_width;
-int block_heigh;
+int block_height;
 
 // 0 = nada; 1 = wall; 2 = player
 int map[map_size * map_size] = {
@@ -26,17 +26,32 @@ char buff[res_x * res_y];
 void init()
 {    
     for(int i=0; i<res_x * res_y; i++){
-        buff[i] = void_char;
+        buff[i] = '-';
     }
 
     block_width = (int) res_x / map_size;
-    block_heigh = (int) res_y / map_size;
+    block_height = (int) res_y / map_size;
 }
 
 void draw() // função q desenha no buffer, nao mostra na tela apenas coloca a disposição das coisas no buffer
 {
-    for(int i=0; i<map_size * map_size; i++){
-        
+    char fillchar;
+    for(int i=0; i<map_size; i++){
+        for(int j=0; j<map_size; j++){
+            int aux = map[i * map_size + j];
+            if(aux == 0){
+                fillchar = void_char;
+            }
+            if(aux == 1){
+                fillchar = wall_char;
+            }
+
+            for (int y = i * block_height; y < (i + 1) * block_height; y++) {
+                for (int x = j * block_width; x < (j + 1) * block_width; x++) {
+                    buff[y * res_x + x] = fillchar;
+                }
+            }
+        }
     }
 
 }
@@ -50,7 +65,7 @@ void display() // função q de fato desenha as coisas na tela com cor e tudo ma
 
 int main(){
     init();
-
+    draw();
     display();
     
 
